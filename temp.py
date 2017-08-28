ee59defa1dce5dbb404f668517d676010ffa76f7
Pedro <Pedro@MacBook-Pro-de-Pedro.local>
2017-08-28 13:03:48 -0500

    temp

--- temp.py
+++ temp.py
@@ -1,11 +1,14 @@
-#Python usa librerias como matplotlib con objetos predeterminados como pyplot que tienen propiedades que permiten enviar las instrucciones a la maquina de una forma resumida. Matplotlib es una libreria para crear graficas en 2 dimensiones. 
+#Python usa librerias como matplotlib con objetos predeterminados como pyplot que tienen propiedades que permiten enviar las instrucciones $
 import matplotlib.pyplot as plt
-#Construimos el objeto plt introduciendo valores que se grafican contra el numero de entrada. Por ejemplo si queremos graficar el crecimiento de un material por dia en centimetros. 
-plt.plot([1,2,3,4])
-#Con esta isntruccion colocamos un titulo al eje Y de la grafica. 
-plt.ylabel('Crecimiento del material X por dia [cm]')
+#Construimos el objeto plt introduciendo valores que se grafican contra el numero de entrada. Por ejemplo si queremos graficar el crecimien$
+plt.plot([5,6,8,10,13,13,13,13,15,15,15,15,15,15,15,15,16,16,16,17,18,18])
+#Con esta isntruccion colocamos un titulo al eje Y de la grafica.
+plt.ylabel('Number of cousins and brothers')
+plt.xlabel('Years of life')
 #Con esta isntruccion guardamos la imagen con el formato que queramos.
-plt.savefig('temp.png')
-#Con esta instruccion mostramos la figura. 
+plt.savefig('tarea1.png')
+#Con esta instruccion mostramos la figura.
 plt.show()
+
+
 
